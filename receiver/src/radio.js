a66f671c182004b341606383c4af3ff9bc7bedf8
yogspace <maximilian.h.weber@stud.h-da.de>
2021-02-05 11:42:52 +0100

    changed filenames

--- receiver/src/radio.js
+++ receiver/src/radio.js
@@ -1,23 +1,13 @@
 'use strict';
 
 const rf=require("nrf24");
-const Gpio = require("pigpio").Gpio;
-
-
-//LED
-const led = new Gpio(17, {mode: Gpio.OUTPUT});
-//Vibrationsmotor
-const vMotor = new Gpio(27, {mode: Gpio.INPUT});
-//Button
-const button = new Gpio(4, {
-    mode: Gpio.INPUT,
-    pullUpDown: Gpio.PUD_DOWN,
-    edge: Gpio.EITHER_EDGE
-});
+const readline = require('readline');
 
 const CE=22,CS=0;
+//const CE=25,CS1=1;
 const IRQ=-1;
-//console.log("Cofiguration pins CE->",CE," CS->",CS," IRQ->",IRQ);
+//const IRQ=27;
+console.log("Cofiguration pins CE->",CE," CS->",CS," IRQ->",IRQ);
 var radio=new rf.nRF24(CE,CS);
 
 radio.begin(false);
@@ -37,49 +27,88 @@ var config={PALevel:rf.RF24_PA_MIN,
           };
 
 
-radio.config(config,false);
-const pipe = "0x65646f4e32" 
-radio.addReadPipe(pipe);
+radio.config(config,true);
 
-let setInt = setInterval(loop,1000);
-
-function loop(){
-  rcv();
-}
+var Pipes= ["0x65646f4e32","0x65646f4e31"]; // "1Node", "2Node"
 
 function rcv(){
-  if (radio.read){
-    warning(false);
-  }
-  else {
-    warning(false);
-  }
-  console.log(radio.read())
+
+  var rpipe=radio.addReadPipe(Pipes[0]);
+  console.log("Read Pipe "+ rpipe + " opened for " + Pipes[0]);
+  radio.useWritePipe(Pipes[1]);
   radio.read(function(d,items) {
-    let data = d[0].data.toString();
-      console.log(data);
+    /*
+    for(var i=0;i<items;i++){
+      let r = d[i].data.readUInt32LE(0);
+      //let r=d[i].data.readUInt32LE(0); // lese das [i] item als Int
+      console.log("Payload Rcv ["+ charCodeAt +"], reply result:" + radio.write(d[i].data));
+    }
+    */
+   console.log(d[0].data.toString() + "\n" + items);
   },function() { console.log("STOP!"); });
+
 }
 
-function warning(bool){
-  if (bool === true){
-  led.pwmWrite(255);
-  vMotor.pwmWrite(255);
+function snd(do_sync) {
+
+  radio.useWritePipe(Pipes[1]);
+  radio.addReadPipe(Pipes[0]);
+  console.log("Open Write Pipe "+ Pipes[1]+ " reading from " + Pipes[0]);
+
+  var tmstp;
+  var roundtrip;
+  var sender=function(){
+    var data=Buffer.alloc(4);
+    tmstp=Math.round(+new Date()/1000);
+    data.writeUInt32LE(tmstp);
+    process.stdout.write("\nSending....");
+    roundtrip=process.hrtime();
+    if(do_sync) {
+       if(radio.write(data)) process.stdout.write("[Sync] Sended " + tmstp);
+       else process.stdout.write("[Sync] Failed to send " + tmstp +" ");
+    } else {
+      radio.write(data,function(success){
+        if(success) process.stdout.write("[Async] Sended " + tmstp);
+        else process.stdout.write("[Async] Failed to send " + tmstp +" ");
+      });
+    }
+  };
+  radio.read(function(d,n) {
+    if(n==1){
+      let t=process.hrtime(roundtrip);
+      process.stdout.write(" | response received |")
+      let ret=d[0].data.readUInt32LE(0);
+      if( ret == tmstp) process.stdout.write(" reponse matchs ");
+      else process.stdout.write(" response does not match "+ ret + " != " + tmstp);
+      console.log("| roundtrip took", (t[1] /1000).toFixed(0)," us");
+    }
+  },function(){ console.log("STOPPED!"); });
+
+  setInterval(sender,1000); // send every 1 sec.
+
 }
-  else{
-    led.pwmWrite(0);
-    vMotor.pwmWrite(0);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("RF24 GettingStarted");
+console.log("This script assumes that this radio is radionumber=1 in ardunino.");
+console.log("Other radio must be configured with radionumber=0")
+rl.question("[s]ender or [R]eceiver?",(resp)=>{
+  var mode=resp.charAt(0);
+  if(mode.toUpperCase()=="S") {
+    rl.question("Send [s]ync or [A]sync?",(resp2)=>{
+       var asy=resp2.charAt(0);
+       console.log("Starting snd... press CTRL+C to stop");
+       if(asy.toUpperCase()=="S") snd(true);
+       else snd(false);
+       rl.close(); 
+    }); 
+  }else {
+    console.log("Starting rcv.... press CTRL+C to stop");
+    rcv();
+    rl.close();
   }
-}
-button.on("interrupt", (level) => {
-  console.log("bStatus: " + level);
-  //endLoop();
-  warning(false);
-  });
-
-function clearInt() {
-    clearInterval(setInt);
-}
-
-//ctrl + c in node
-process.on("SIGINT",clearInt);
+});
