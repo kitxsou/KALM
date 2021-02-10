7995aad194471266dd9ce1f0d42776fca730ead3
Phuong Quach <phuongkittyquach@gmail.com>
2021-02-10 23:28:59 +0100

    Added radio socket and rescue track integration

--- ev_node/src/hardware.js
+++ ev_node/src/hardware.js
@@ -1,6 +1,48 @@
+const http = require("http").Server();
+const { Server } = require("socket.io");
+const { RoutePoint } = require("./route_point");
+
+// will be initialized by calling startRadio
+let io;
+
+function startRadio() {
+  // Create a new socket io server
+  io = new Server(http);
+
+  // This method is called when a client connects
+  io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    // This method is called once the client disconnects
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
+    });
+  });
+
+  // Starts the http server on port 3000
+  http.listen(3000, () => {
+    console.log("listening on http://localhost:3000");
+  });
+}
+
+function sendWithRadio(data) {
+  console.log(`Sending data:`);
+  console.log(data);
+
+  // sends the provided object as json using socket
+  io.emit("data", data);
+}
+
 function getGpsData() {
+  // Cannot use gps module if running on windows
+  const isWindows = process.platform === "win32";
+  if (isWindows) {
+    return new RoutePoint(49.886545, 8.841271);
+  }
+
+  return new Promise((resolve, reject) => {
     // returns route point of current location from gps module
-  
+
     // Tutorial used: https://www.youtube.com/watch?v=ijfBeMTuWhU
 
     const SerialPort = require("serialport"); // this library ensures we can interface with the gps module (which gets "raw" data)
@@ -8,7 +50,7 @@ function getGpsData() {
 
     const gpsAPI = require("gps"); // allows to parse the "raw" data coming from the gps
 
-    const port = new SerialPort("/dev/ttyS0", {baudRate: 9600}); // connects to the serial port the data is coming in
+    const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 }); // connects to the serial port the data is coming in
     const gpsParser = new gpsAPI(); // initialise gps Parser
 
     let latitudeLong = "";
@@ -17,87 +59,86 @@ function getGpsData() {
     let longitudeShort = "";
 
     let routePoint = {
-    "lat": "0",
-    "lon": "0"
-    }
+      lat: "0",
+      lon: "0",
+    };
 
     const parser = port.pipe(new SerialPortParser()); // data coming in will be piped into the variable
 
-    gpsParser.on("data", data => { // listens for the data updated by the Serial Port Parser into the gps parser
-        if(data.type === "GGA") { // just the relevant data types, ignores the rest
-            if(data.quality !== null) { // check if there is something in GGA; GGA is part of the output we get from the gps module 
-                latitudeLong = data.lat;
-                latitudeShort = latitudeLong.toString().substring(0, 9); // substring shortens the string so the whole JSON can be send by radio
-                routePoint.lat = latitudeShort;
+    gpsParser.on("data", (data) => {
+      // listens for the data updated by the Serial Port Parser into the gps parser
+      if (data.type === "GGA") {
+        // just the relevant data types, ignores the rest
+        if (data.quality !== null) {
+          // check if there is something in GGA; GGA is part of the output we get from the gps module
+          latitudeLong = data.lat;
+          latitudeShort = latitudeLong.toString().substring(0, 9); // substring shortens the string so the whole JSON can be send by radio
+          routePoint.lat = latitudeShort;
 
+          longitudeLong = data.lon;
+          longitudeShort = longitudeLong.toString().substring(0, 9);
+          routePoint.lon = longitudeShort;
 
-                longitudeLong = data.lon;
-                longitudeShort = longitudeLong.toString().substring(0, 9);
-                routePoint.lon = longitudeShort;
-                
-                return routePoint;
-            }
+          resolve(routePoint);
         }
+      }
     });
 
     // listening for data coming from the Serial Port into that parser
-    parser.on("data", data => {
-        gpsParser.update(data);
-    }); 
-    
-}
-
-function sendWithRadio(data) {
-  // sends the provided object as json via radio
+    parser.on("data", (data) => {
+      gpsParser.update(data);
+    });
+  });
 }
 
 function buttonPressed() {
-    // function for use of button
-    // if button is pressed, whole process on rv gets started
-    const Gpio = require('pigpio').Gpio; // initialise module to address gpio pins on the RPi
+  // function for use of button
+  // if button is pressed, whole process on rv gets started
+  const Gpio = require("pigpio").Gpio; // initialise module to address gpio pins on the RPi
 
-    // Button at GPIO 17 
-    const button = new Gpio(17, {
-      mode: Gpio.INPUT, // listens for input (press of the button)
-      pullUpDown: Gpio.PUD_DOWN, // power is just running while button is actively pressed, sends signal
-      edge: Gpio.EITHER_EDGE // power is also recognized while rising or falling
-    });
+  // Button at GPIO 17
+  const button = new Gpio(17, {
+    mode: Gpio.INPUT, // listens for input (press of the button)
+    pullUpDown: Gpio.PUD_DOWN, // power is just running while button is actively pressed, sends signal
+    edge: Gpio.EITHER_EDGE, // power is also recognized while rising or falling
+  });
 
-    // suggestion: if processRunning = true whole process is running, else it is stopped
-    let processRunning = false;
+  // suggestion: if processRunning = true whole process is running, else it is stopped
+  let processRunning = false;
 
-    button.on('interrupt', (level) => {
-      if(processRunning) {
-          processRunning = false;
-      } else { 
-          processRunning = true;
-      }
-   
-      return processRunning;
-    });
+  button.on("interrupt", (level) => {
+    if (processRunning) {
+      processRunning = false;
+    } else {
+      processRunning = true;
+    }
+
+    return processRunning;
+  });
 }
 
-function setColorOfLed(r,g,b) {
-    //color values are reversed (255 = dark, 0 = bright)
-        
-    const Gpio = require('pigpio').Gpio; // initialise module to address gpio pins on the RPi
+function setColorOfLed(r, g, b) {
+  //color values are reversed (255 = dark, 0 = bright)
 
-    // Each color of the LED has an own gpio pin
-    const redPin = new Gpio(16, {mode: Gpio.OUTPUT}); // Output: RPi sends signal to the pin
-    const greenPin = new Gpio(26, {mode: Gpio.OUTPUT});
-    const bluePin = new Gpio(6, {mode: Gpio.OUTPUT});
-    
-    // set Color of each pin
-    redPin.pwmWrite(r); // pwm is used to use values (0 to 255) other than just on and off (like digitalWrite) 
-    greenPin.pwmWrite(g);
-    bluePin.pwmWrite(b);
-    
-    // if LED should be shut down use 255 for each value. Otherwise LED will be on all the time (no matter if program is running or not)
+  const Gpio = require("pigpio").Gpio; // initialise module to address gpio pins on the RPi
+
+  // Each color of the LED has an own gpio pin
+  const redPin = new Gpio(16, { mode: Gpio.OUTPUT }); // Output: RPi sends signal to the pin
+  const greenPin = new Gpio(26, { mode: Gpio.OUTPUT });
+  const bluePin = new Gpio(6, { mode: Gpio.OUTPUT });
+
+  // set Color of each pin
+  redPin.pwmWrite(r); // pwm is used to use values (0 to 255) other than just on and off (like digitalWrite)
+  greenPin.pwmWrite(g);
+  bluePin.pwmWrite(b);
+
+  // if LED should be shut down use 255 for each value. Otherwise LED will be on all the time (no matter if program is running or not)
 }
 
 module.exports = {
   getGpsData,
   sendWithRadio,
   buttonPressed,
-  setColorOfLed
+  setColorOfLed,
+  startRadio,
 };
--- ev_node/src/main.js
+++ ev_node/src/main.js
@@ -1,41 +1,60 @@
 const { RoutePoint } = require("./route_point");
-const { getGpsData, sendWithRadio } = require("./hardware");
+const { getGpsData, sendWithRadio, startRadio } = require("./hardware");
+const fetch = require("node-fetch");
+const { v4: uuidv4 } = require("uuid");
 
 console.log("Running...");
 const id = generateId();
-const destinationLocation = new RoutePoint(
-  49.87292059988813,
-  8.656861782073976
-);
+// https://goo.gl/maps/9qMrHZfTZEYFB6L97
+const destinationLocation = new RoutePoint(49.886545, 8.841271);
 
-setInterval(updateData, 2000); // updates data every 2s
+startRadio();
 
-function updateData() {
+// updates data every 10s
+setInterval(() => updateData().catch(console.error), 10 * 1000);
+
+async function updateData() {
   console.log("Refreshing...");
 
   // creates data object with id and current location taken from gps module
   const data = {
     evId: id,
-    currentLocation: getGpsData(),
+    currentLocation: await getGpsData(),
   };
 
   // gets route from current location to destination location
-  data.route = getRoute(data.currentLocation, destinationLocation);
+  data.route = await getRoute(data.currentLocation, destinationLocation);
 
   sendWithRadio(data);
 }
 
-function getRoute(startPoint, endPoint) {
-  // calls rescue-track and extracts route points
-  const rescueTrackData = getRescueTrackData(startPoint, endPoint);
+// calls rescue-track and extracts route points
+// returns array of route points
+async function getRoute(startPoint, endPoint) {
+  // returns rescue track data as object
+  const rescueTrackData = await getRescueTrackData(startPoint, endPoint);
 
-  // returns array of route points
+  const allRoutePoints = rescueTrackData.item1;
+
+  return allRoutePoints.splice(0, 3);
 }
 
-function getRescueTrackData(startPoint, endPoint) {
-  // https://apps.rescuetrack.com/api/routing/v3/spatial/route/48.518317061824256/9.05802369117737/49.87292059988813/8.656861782073976
+async function getRescueTrackData(startPoint, endPoint) {
+  const api = "https://apps.rescuetrack.com";
+  const url = `${api}/api/routing/v3/spatial/route/${startPoint.lat}/${startPoint.lng}/${endPoint.lat}/${endPoint.lng}`;
+
+  console.log(`RescueTrack: ${url}`);
+  const response = await fetch(url);
+  if (response.status != 200) {
+    // throw error if response status is not Ok
+    throw Error("RescueTrack error");
+  }
+
+  const json = await response.json();
+
+  return json;
 }
 
 function generateId() {
-  // returns random id
+  return uuidv4(); // create unique user id from uuid npm package
 }
